3850a7838b65b25034397ad7c23f0bef94d775ac
ossydotpy <mrali.osman.6@gmail.com>
2023-04-15 18:11:44 +0000

    restructure

--- cogs/cogs/prio_cog.py
+++ cogs/cogs/prio_cog.py
@@ -14,7 +14,7 @@ class PRIOCog(commands.Cog):
 
     @staticmethod
     def retrieve_features():
-        with open('data/prio_features.json', 'r') as features:
+        with open('functions/prio_features.json', 'r') as features:
             data = json.load(features)
         return data
 
@@ -36,40 +36,27 @@ class PRIOCog(commands.Cog):
                     )
                     embed.set_author(name=f"Requested by {ctx.author.display_name}")
                     for result in results:
-
-                        stats_left = (
-                        f"Defense:\t\t**{result['defense']}**\n"
-                        f"Strength:\t\t**{result['strength']}**\n"
-                        f"Dexterity:\t\t**{result['dexterity']}**\n"
-                        f"Perception:\t\t**{result['perception']}**\n"
-                    )
-                        stats_right = (
-                            f"Constitution:\t\t**{result['constitution']}**\n"
-                            f"Hit Points:\t\t**{result['hit_points']}**\n"
-                            f"V. Handling:\t**{result['vehicle_handling']}**\n"
-                        )
-
-                        stats_combined = '\n'.join([f"{stats_left.splitlines()[i]}\t\t{stats_right.splitlines()[i]}" for i in range(3)])
-                        embed.set_image(url=result['image'])
-
                         embed.add_field(
                             name='Monster Bio', 
-                            value=f"Class: **{result['class']}**\n"
+                            value=f"Name: **{result['name']}**\n"
+                            f"Class: **{result['class']}**\n"
                             f"Birth State: **{result['birth_state']}**"
                         )
-
                         embed.add_field(
                             name='Monster Stats',
-                            value=stats_combined,
-                            inline=False  
+                            value=f"Defense:\t\t\t\t\t**{result['defense']}**\n"
+                                f"Strength:\t\t\t\t\t**{result['strength']}**\n"
+                                f"Dexterity:\t\t\t\t\t**{result['dexterity']}**\n"
+                                f"Hit Points:\t\t\t\t**{result['hit_points']}**\n"
+                                f"Perception:\t\t\t\t**{result['perception']}**\n"
+                                f"Constitution:\t\t\t**{result['constitution']}**\n"
+                                f"Vehicle Handling:\t**{result['vehicle_handling']}**\n",
+                            inline=False
                         )
-                        
                         embed.add_field(
                                 name='Summary', 
-                                value=f"Total Points: **{result['total']}**\n"
-                                f"Ratings:\t\t **{round((result['total_minus_hp']/result['max'])*100,2)}%**"
+                                value=f"Total Points: **{result['total']}**"
                             )
-                        
                         embed.set_thumbnail(url='https://hermonsters.com/core/views/96a589a588/assets/images/hm_logo.png')
                         embed.set_footer(text='Powered by noob')
                     await ctx.send(embed=embed)
--- cogs/cogs/proto_cog.py
+++ cogs/cogs/proto_cog.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 import discord
 import datetime
 
-class PRIOCog(commands.Cog):
+class PROTOCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
@@ -14,12 +14,12 @@ class PRIOCog(commands.Cog):
 
     @staticmethod
     def retrieve_features():
-        with open('data/prio_features.json', 'r') as features:
+        with open('functions/proto_features.json', 'r') as features:
             data = json.load(features)
         return data
 
     @commands.command()
-    async def prio(self, ctx, keyword: str):
+    async def proto(self, ctx, keyword: str):
         nfts = self.retrieve_features()
         if keyword.startswith('#'):
             exact_match = re.match(r'^#(\d+)$', keyword)
@@ -36,40 +36,27 @@ class PRIOCog(commands.Cog):
                     )
                     embed.set_author(name=f"Requested by {ctx.author.display_name}")
                     for result in results:
-
-                        stats_left = (
-                        f"Defense:\t\t**{result['defense']}**\n"
-                        f"Strength:\t\t**{result['strength']}**\n"
-                        f"Dexterity:\t\t**{result['dexterity']}**\n"
-                        f"Perception:\t\t**{result['perception']}**\n"
-                    )
-                        stats_right = (
-                            f"Constitution:\t\t**{result['constitution']}**\n"
-                            f"Hit Points:\t\t**{result['hit_points']}**\n"
-                            f"V. Handling:\t**{result['vehicle_handling']}**\n"
-                        )
-
-                        stats_combined = '\n'.join([f"{stats_left.splitlines()[i]}\t\t{stats_right.splitlines()[i]}" for i in range(3)])
-                        embed.set_image(url=result['image'])
-
                         embed.add_field(
                             name='Monster Bio', 
-                            value=f"Class: **{result['class']}**\n"
+                            value=f"Name: **{result['name']}**\n"
+                            f"Class: **{result['class']}**\n"
                             f"Birth State: **{result['birth_state']}**"
                         )
-
                         embed.add_field(
                             name='Monster Stats',
-                            value=stats_combined,
-                            inline=False  
+                            value=f"Defense:\t\t\t\t\t**{result['defense']}**\n"
+                                f"Strength:\t\t\t\t\t**{result['strength']}**\n"
+                                f"Dexterity:\t\t\t\t\t**{result['dexterity']}**\n"
+                                f"Hit Points:\t\t\t\t**{result['hit_points']}**\n"
+                                f"Perception:\t\t\t\t**{result['perception']}**\n"
+                                f"Constitution:\t\t\t**{result['constitution']}**\n"
+                                f"Vehicle Handling:\t**{result['vehicle_handling']}**\n",
+                            inline=False
                         )
-                        
                         embed.add_field(
                                 name='Summary', 
-                                value=f"Total Points: **{result['total']}**\n"
-                                f"Ratings:\t\t **{round((result['total_minus_hp']/result['max'])*100,2)}%**"
+                                value=f"Total Points: **{result['total']}**"
                             )
-                        
                         embed.set_thumbnail(url='https://hermonsters.com/core/views/96a589a588/assets/images/hm_logo.png')
                         embed.set_footer(text='Powered by noob')
                     await ctx.send(embed=embed)
@@ -80,5 +67,5 @@ class PRIOCog(commands.Cog):
 
 
 async def setup(bot):
-    await bot.add_cog(PRIOCog(bot))
+    await bot.add_cog(PROTOCog(bot))
 
