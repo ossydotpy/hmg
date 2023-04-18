ca432b6395d7e489defa33c2339a1ff58f30b665
ossydotpy <mrali.osman.6@gmail.com>
2023-04-18 13:26:36 +0000

    formatting

--- cogs/prio_cog.py
+++ cogs/prio_cog.py
@@ -4,79 +4,83 @@ from discord.ext import commands
 import discord
 import datetime
 
+
 class PRIOCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.Cog.listener()
     async def on_ready(self):
-        print ('nft cog ready')
+        print("nft cog ready")
 
     @staticmethod
     def retrieve_features():
-        with open('data/prio_features.json', 'r') as features:
+        with open("data/prio_features.json", "r") as features:
             data = json.load(features)
         return data
 
     @commands.command()
+    @commands.cooldown(1, 20, commands.BucketType.user)
     async def prio(self, ctx, keyword: str):
         nfts = self.retrieve_features()
-        if keyword.startswith('#'):
-            exact_match = re.match(r'^#(\d+)$', keyword)
+        if keyword.startswith("#"):
+            exact_match = re.match(r"^#(\d+)$", keyword)
             if exact_match:
                 number = int(exact_match.group(1))
-                results = [d for d in nfts if int(d['name'].split()[-1][1:]) == number]
+                results = [d for d in nfts if int(d["name"].split()[-1][1:]) == number]
             else:
-                results = [d for d in nfts if keyword.lower() in d['name'].lower()]
+                results = [d for d in nfts if keyword.lower() in d["name"].lower()]
             if results:
-                    embed = discord.Embed(
-                        title="NFT Search Results", 
-                        description="Here are the search results for the requested NFT:",
-                        timestamp=datetime.datetime.utcnow()
-                    )
-                    embed.set_author(name=f"Requested by {ctx.author.display_name}")
-                    for result in results:
-                        embed.add_field(
-                                name='Name:', 
-                                value=f"**{result['name']}**\n"
-                            )
+                embed = discord.Embed(
+                    title="NFT Search Results",
+                    description="Here are the search results for the requested NFT:",
+                    timestamp=datetime.datetime.utcnow(),
+                )
+                embed.set_author(name=f"Requested by {ctx.author.display_name}")
+                for result in results:
+                    embed.add_field(name="Name:", value=f"**{result['name']}**\n")
 
-                        stats_left = (
+                    stats_left = (
                         f"Defense:\t\t**{result['defense']}**\n"
                         f"Strength:\t\t**{result['strength']}**\n"
                         f"Dexterity:\t\t**{result['dexterity']}**\n"
                         f"Perception:\t\t**{result['perception']}**\n"
                     )
-                        stats_right = (
-                            f"Constitution:\t\t**{result['constitution']}**\n"
-                            f"Hit Points:\t\t**{result['hit_points']}**\n"
-                            f"V. Handling:\t**{result['vehicle_handling']}**\n"
-                        )
+                    stats_right = (
+                        f"Constitution:\t\t**{result['constitution']}**\n"
+                        f"Hit Points:\t\t**{result['hit_points']}**\n"
+                        f"V. Handling:\t**{result['vehicle_handling']}**\n"
+                    )
 
-                        stats_combined = '\n'.join([f"{stats_left.splitlines()[i]}\t\t{stats_right.splitlines()[i]}" for i in range(3)])
-                        embed.set_image(url=result['image'])
+                    stats_combined = "\n".join(
+                        [
+                            f"{stats_left.splitlines()[i]}\t\t{stats_right.splitlines()[i]}"
+                            for i in range(3)
+                        ]
+                    )
+                    embed.set_image(url=result["image"])
 
-                        embed.add_field(
-                            name='Monster Bio', 
-                            value=f"Class: **{result['class']}**\n"
-                            f"Birth State: **{result['birth_state']}**"
-                        )
+                    embed.add_field(
+                        name="Monster Bio",
+                        value=f"Class: **{result['class']}**\n"
+                        f"Birth State: **{result['birth_state']}**",
+                    )
 
-                        embed.add_field(
-                            name='Monster Stats',
-                            value=stats_combined,
-                            inline=False  
-                        )
-                        
-                        embed.add_field(
-                                name='Summary', 
-                                value=f"Total Points: **{result['total']}**\n"
-                                f"Ratings:\t\t **{round((result['total_minus_hp']/result['max'])*100,2)}%**"
-                            )
-                        
-                        embed.set_thumbnail(url='https://hermonsters.com/core/views/96a589a588/assets/images/hm_logo.png')
-                        embed.set_footer(text='Powered by noob')
-                    await ctx.send(embed=embed)
+                    embed.add_field(
+                        name="Monster Stats", value=stats_combined, inline=False
+                    )
+
+                    embed.add_field(
+                        name="Summary",
+                        value=f"Total Points: **{result['total']}**\n"
+                        f"Ratings:\t\t **{round((result['total_minus_hp']/result['max'])*100,2)}%**",
+                    )
+
+                    embed.set_thumbnail(
+                        url="https://hermonsters.com/core/views/96a589a588/assets/images/hm_logo.png"
+                    )
+                    embed.set_footer(text="Powered by noob")
+                await ctx.send(embed=embed)
             else:
                 await ctx.send(f"No results found for query: {keyword}")
         else:
@@ -85,4 +89,3 @@ class PRIOCog(commands.Cog):
 
 async def setup(bot):
     await bot.add_cog(PRIOCog(bot))
-
--- main.py
+++ main.py
@@ -10,57 +10,80 @@ from discord import Activity, ActivityType
 load_dotenv()
 
 # Get bot token from environment variable
-TOKEN = os.getenv('BOT_TOKEN')
+TOKEN = os.getenv("BOT_TOKEN")
 
 # Define intents
 intents = discord.Intents.all()
 
 # Create bot instance
-bot = commands.Bot(command_prefix='-', intents=intents,help_command=None)
+bot = commands.Bot(command_prefix="-", intents=intents, help_command=None)
 
 
 # Load cogs on startup
 @bot.event
 async def on_ready():
-    print('Bot is ready.')
-    await bot.change_presence(activity=Activity(type=ActivityType.listening, name='your mom moan'))
-    for filename in os.listdir('./cogs'):
-        if filename.endswith('.py'):
+    print("Bot is ready.")
+    await bot.change_presence(
+        activity=Activity(type=ActivityType.listening, name="your mom moan")
+    )
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
             try:
-                await bot.load_extension(f'cogs.{filename[:-3]}')
-                print(f'{filename[:-3]} loaded successfully.')
+                await bot.load_extension(f"cogs.{filename[:-3]}")
+                print(f"{filename[:-3]} loaded successfully.")
             except Exception as e:
-                print(f'Error loading {filename}: {e}')
+                print(f"Error loading {filename}: {e}")
+
 
 @bot.command()
+@commands.cooldown(1, 20, commands.BucketType.user)
 async def statsguide(ctx):
-    embed = discord.Embed(title="Command List", description="how do i use the bot ser?", color=0x00ff00)
+    embed = discord.Embed(
+        title="Command List", description="how do i use the bot ser?", color=0x00FF00
+    )
 
-    embed.add_field(name="-mmm #asset_number", value="query Militia NFT"
-    "eg: -mmm #1 will display HM Monster #1", inline=False)
-    embed.add_field(name="-prio #asset_number", value="query Prio Gang NFT"
-    "eg: -prio #1 will display HM Prio Monster #1", inline=False)
-    embed.add_field(name="-proto #asset_number", value="query Prototype NFT"
-    "eg: -proto #1 will display HM Prototype #1", inline=False)
+    embed.add_field(
+        name="-mmm #asset_number",
+        value="query Militia NFT" "eg: -mmm #1 will display HM Monster #1",
+        inline=False,
+    )
+    embed.add_field(
+        name="-prio #asset_number",
+        value="query Prio Gang NFT" "eg: -prio #1 will display HM Prio Monster #1",
+        inline=False,
+    )
+    embed.add_field(
+        name="-proto #asset_number",
+        value="query Prototype NFT" "eg: -proto #1 will display HM Prototype #1",
+        inline=False,
+    )
+    embed.set_image(
+        url="https://th.bing.com/th/id/OIP.efhlBG7w2NmutSTuPUzZiQHaDH?pid=ImgDet&rs=1"
+    )
 
     await ctx.send(embed=embed)
+
 
 # Catch errors and throw helpful explanations
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.errors.CommandNotFound):
-        await ctx.send('Invalid command. Type !help for a list of available commands.')
+        await ctx.send("Invalid command. Type !help for a list of available commands.")
     elif isinstance(error, commands.errors.MissingRequiredArgument):
-        await ctx.send('Missing required argument.')
+        await ctx.send("Missing required argument.")
     elif isinstance(error, commands.errors.CommandOnCooldown):
-        await ctx.send(f'This command is on cooldown. Please try again in {error.retry_after:.2f} seconds.')
+        await ctx.send(
+            f"This command is on cooldown. Please try again in {error.retry_after:.2f} seconds."
+        )
     else:
-        await ctx.send('An error occurred while executing the command.')
+        await ctx.send("An error occurred while executing the command.")
 
 
 tracemalloc.start()
 
-async def main():# Run the bot
-    await bot.start(TOKEN)
-asyncio.run(main())
 
+async def main():  # Run the bot
+    await bot.start(TOKEN)
+
+
+asyncio.run(main())
